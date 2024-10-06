9cc701a6616459188f6dd357d5e7cc29486a5c83
bulbulahammedjibon <bulbulahammedgmail.com>
2024-10-06 23:03:17 +0600

    delete all unused line of code

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.use(express.json());
 
 
 // z6vHAS2EaQe1hLs7
-// real-state
+
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fxm2qso.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
